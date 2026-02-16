b8d1720367f0312a77eac84e14a237babd837316
derek-hylee <derek@hy-lee.com>
2026-02-16 07:17:22 -0600

    fix(footer): increase margin below newsletter heading (mb-8 -> mb-10)

--- hydrogen/app/components/layout/Footer.tsx
+++ hydrogen/app/components/layout/Footer.tsx
@@ -71,7 +71,7 @@ function NewsletterSignup() {
 
   return (
     <div className="text-center">
-      <h3 className="text-xl font-normal text-text mb-8">
+      <h3 className="text-xl font-normal text-text mb-10">
         Sign Up for HyLee news &amp; updates!
       </h3>
       <form
